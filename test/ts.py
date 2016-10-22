9b8ee486f915aa377f4c00b7f8ead0dfd5e59b08
johnoneil <oneil.john@gmail.com>
2016-10-22 14:17:12 -0700

    Added data access from packet test.

--- test/ts.py
+++ test/ts.py
@@ -12,7 +12,29 @@ import sys
 import argparse
 
 PACKET_SIZE = 188
+# sync byte
+SYNC_BYTE_INDEX = 0
 SYNC_BYTE = 'G'
+# Transport Error Indicator (TEI)
+TEI_INDEX = 1
+TEI_MASK = 0x80
+# Payload Unit Start Indicator (PUSI)
+PUSI_INDEX = 1
+PUSI_MASK = 0x40
+#Packt ID (PID)
+PID_START_INDEX = 1
+PID_LENGTH_BYTES = 2
+PID_MASK = 0x1fff
+
+
+def check_packet_formedness(packet):
+  """Check some features of this packet and see if it's well formed or not
+  """
+  if len(packet) != PACKET_SIZE:
+    raise Exception("Provided input packet string not of correct size")
+
+  if packet[0] != SYNC_BYTE:
+    raise Exception("Provided input packet does not begin with correct sync byte.")
 
 #generator
 def next_packet(filename):
@@ -39,6 +61,19 @@ def next_packet(filename):
         yield packet
       else:
         break
+
+def get_transport_error_indicator(packet):
+  return (ord(packet[TEI_INDEX]) & TEI_MASK) == 1
+
+def get_payload_start(packet):
+  return (ord(packet[PUSI_INDEX]) & PUSI_MASK) == 1
+
+def get_pid(packet):
+  """Given a stringified MPEG TS packet, extract the PID value
+  and return it as a simple integer value.
+  Do this as quickly as possible for performance
+  """
+  return ((ord(packet[PID_START_INDEX]) & 0x1f)<<8) | ord(packet[PID_START_INDEX+1])
 
 def main():
   parser = argparse.ArgumentParser(description='Remove ARIB formatted Closed Caption information from an MPEG TS file and format the results as a standard .ass subtitle file.')
@@ -72,6 +107,12 @@ def main():
       prev_percent_read = new_percent_read
       sys.stdout.write("progress: %.2f%%   \r" % (percent_read) )
       sys.stdout.flush()
+    
+    # extract data from packet to do a "worst case" access test
+    check_packet_formedness(packet)
+    pei = get_transport_error_indicator(packet)
+    pusi = get_payload_start(packet)
+    pid = get_pid(packet)
     
 if __name__ == "__main__":
   main()
